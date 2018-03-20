cbf207512da45872ac431f68919f2d28b6f07d2f
Brandon Allred <b32alls@gmail.com>
2018-03-20 15:30:11 -0600

    changes to get the app working properly

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -1,42 +1,41 @@
 const users = require('../models/users');
 let id = 1;
 
-// var user = {
-//     id: integer,
-//     username:string,
-//     password:string
-// }
-
 module.exports = {
-    login: (req, res, next) => {
-        const { username, password } = req.body
-        const { session } = req
+  login: ( req, res, next ) => {
+    const { session } = req;
+    const { username, password } = req.body;
 
-        const user = users.find(user => user.username === username && user.password === password);
-        if (user) {
-            session.user.username = user.username;
-            res.status(200).json(session.user);
-        } else {
-            res.status(500).json('Unauthorized')
-        }
-    },
-    register: (req, res, next) => {
-        const { session } = req
-        const { username, password } = req.body
+    const user = users.find( user => user.username === username && user.password === password );
 
-        users.push({ id, username, password });
-        id++
-        session.user.username = username
-        res.status(200).json(session.user);
-
-    },
-    signout: (req, res, next) => {
-        const { session } = req
-        session.destroy();
-        res.status(200).json(req.session);
-    },
-    getUser: (req, res, next) => {
-        const { session } = req
-        res.status(200).json(session.user);
+    if ( user ) {
+      session.user.username = user.username;
+      res.status(200).send(session.user);
+    } else {
+      res.status(500).send('Unauthorized.');
     }
-}
+  },
+
+  register: ( req, res, next ) => {
+    const { session } = req;
+    const { username, password } = req.body;
+
+    users.push({ id, username, password });
+    id++;
+
+    session.user.username = username;
+
+    res.status(200).send( session.user );
+  },
+
+  signout: ( req, res, next ) => {
+    const { session } = req;
+    session.destroy();
+    res.status(200).send( req.session );
+  },
+
+  getUser: ( req, res, next ) => {
+    const { session } = req;
+    res.status(200).send( session.user );
+  }
+};
--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -1,39 +1,40 @@
 const swag = require('../models/swag');
-// console.log(swag.find(swag=>swag.id===33))
 
-module.exports= {
-    add: (req,res,next) => {
-        const {id} = req.query;
-        const {cart} = req.session.user;
-        const index = cart.findIndex(swag => swag.id == id);
+module.exports = {
+  add: ( req, res, next ) => {
+    const { id } = req.query;
+    let { cart } = req.session.user;
 
-        if(index === -1) {
-            const selectedSwag = swag.find(swag=>swag.id)
+    const index = cart.findIndex( swag => swag.id == id );
 
-            cart.push(selectedSwag)
-            req.session.user.total += selectedSwag.price
-        
-        }
-        res.status(200).send(req.session.user)
+    if ( index === -1 ) {
+      const selectedSwag = swag.find( swag => swag.id == id );
 
-    },
-    delete: (req,res,next) => {
-        const {id} = req.query
-        const{cart} = req.session.user
-        // console.log(cart)
-        const {selectedSwag} = swag.find(swag=>swag.id)
-        if(selectedSwag) {
-            const i = cart.findIndex(swag=> swag.id == id)
-            cart.splice(i, 1)
-            req.session.user.total -= selectedSwag.price
-        }
-        res.status(200).json(req.session.user)
-    },
-    checkout: (req,res,next) => {
-        const {user} = req.session
-        user.cart = [];
-        user.total = 0
-        
-        res.status(200).json(req.session.user);
+      cart.push( selectedSwag );
+      req.session.user.total += selectedSwag.price;
     }
-}
+
+    res.status(200).send( req.session.user );
+  },
+
+  delete: ( req, res, next ) => {
+    const { id } = req.query;
+    const { cart } = req.session.user;
+
+    if ( selectedSwag ) {
+      const i = cart.findIndex( swag => swag.id == id );
+      cart.splice(i, 1);
+      req.session.user.total -= selectedSwag.price;
+    }
+    
+    res.status(200).send( req.session.user );
+  },
+
+  checkout: ( req, res, next ) => {
+    const { user } = req.session;
+    user.cart = [];
+    user.total = 0;
+
+    res.status(200).send( req.session.user );
+  }
+} 
